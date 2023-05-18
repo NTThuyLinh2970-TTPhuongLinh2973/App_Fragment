47bc6de6862e3517a346f826f430f4274102bfcb
NTThuyLinh2970-TTPhuongLinh2973 <minhnguyen7848@gmail.com>
2023-05-19 01:44:26 +0700

    frag

---
+++ app/src/main/java/com/example/app1/CallLog.java
@@ -0,0 +1,23 @@
+package com.example.app1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CallLog extends Fragment {
+
+    public CallLog() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_call_log, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/app1/Dial.java
@@ -0,0 +1,23 @@
+package com.example.app1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Dial extends Fragment {
+
+    public Dial() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dial, container, false);
+    }
+}
